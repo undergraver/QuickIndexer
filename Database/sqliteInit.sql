fc8e0fd0f7f23215cc3ecc1b9fc22568a071d587
Iulian Serbanoiu <undergraver@gmail.com>
2013-06-19 23:20:08 +0300

    Added trigger to "delete jobs referring deleted paths"

--- Database/sqliteInit.sql
+++ Database/sqliteInit.sql
@@ -3,6 +3,8 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS paths (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT, 
     scanpath TEXT,
+    username TEXT,
+    password TEXT,
     pathtype TEXT,
     globexclusion TEXT,
     regexexclusion TEXT
@@ -12,8 +14,11 @@ CREATE TABLE IF NOT EXISTS scanjobs (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     startdate DATE,
     enddate DATE,
+    status TEXT,
+    result TEXT,
     pathid INTEGER,
-    FOREIGN KEY(pathid) REFERENCES paths(id)
+    FOREIGN KEY(pathid) REFERENCES paths(id),
+    CHECK (status IN ("Working","Done") )
 );
 
 CREATE TABLE IF NOT EXISTS files (
@@ -22,4 +27,10 @@ CREATE TABLE IF NOT EXISTS files (
     scanjobid INTEGER,
     FOREIGN KEY(scanjobid) REFERENCES scanjobs(id)
 );
+
+CREATE TRIGGER IF NOT EXISTS deletejobs_referring_deleted_paths
+AFTER DELETE ON paths
+BEGIN
+    DELETE FROM scanjobs WHERE pathid = OLD.id;
+END;
 
