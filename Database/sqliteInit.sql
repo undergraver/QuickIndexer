3aede3b04964e957b6610d050475ef8acf1e06fd
Iulian Serbanoiu <undergraver@gmail.com>
2013-06-22 17:58:43 +0300

    Created trigger to delete files after scanjob is deleted

--- Database/sqliteInit.sql
+++ Database/sqliteInit.sql
@@ -1,5 +1,8 @@
 PRAGMA foreign_keys = ON;
 
+-- paths table; paths present here are scanned and indexed
+-- into the database
+
 CREATE TABLE IF NOT EXISTS paths (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT, 
     scanpath TEXT,
@@ -9,6 +12,9 @@ CREATE TABLE IF NOT EXISTS paths (
     globexclusion TEXT,
     regexexclusion TEXT
 );
+
+-- scan jobs table; a scan job is the operation of scanning
+-- a path referref by the paths table
 
 CREATE TABLE IF NOT EXISTS scanjobs (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
@@ -21,6 +27,8 @@ CREATE TABLE IF NOT EXISTS scanjobs (
     CHECK (status IN ("Working","Done") )
 );
 
+-- files table; files are stored in the database while scanning
+
 CREATE TABLE IF NOT EXISTS files (
     id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     filepath TEXT,
@@ -28,9 +36,19 @@ CREATE TABLE IF NOT EXISTS files (
     FOREIGN KEY(scanjobid) REFERENCES scanjobs(id)
 );
 
+-- trigger to delete all the jobs associated with a deleted path
+
 CREATE TRIGGER IF NOT EXISTS deletejobs_referring_deleted_paths
 AFTER DELETE ON paths
 BEGIN
     DELETE FROM scanjobs WHERE pathid = OLD.id;
+END;
+
+-- trigger to delete all the files associated with a deleted job
+
+CREATE TRIGGER IF NOT EXISTS delete_files_referring_deleted_jobs
+AFTER DELETE ON scanjobs
+BEGIN
+    DELETE FROM files WHERE scanjobid = OLD.id;
 END;
 
