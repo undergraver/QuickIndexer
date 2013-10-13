7d09682fe8542dad3407b5d32eb1200e6496cea7
Iulian Serbanoiu <undergraver@gmail.com>
2013-10-13 18:10:06 +0300

    Pretty scanpaths display

--- Config/PathConfig.py
+++ Config/PathConfig.py
@@ -4,12 +4,40 @@ def AddScanPath(fileName,path,pathType,user,password,excNameGlob,excNameRegex,ex
     db = GetDbAccessHandle(fileName)
     db.AddScanPath(path,pathType,user,password,excNameGlob,excNameRegex,excPathGlob,excPathRegex)
 
-def GetScanPaths(fileName,pathId):
+def ShowScanPaths(fileName,pathId):
     db = GetDbAccessHandle(fileName)
     paths = db.GetScanPaths(pathId)
 
     buf = ""
     for path in paths:
-        s = "Scan path " + path["id"]
+        s = "Scan path " + str(path["id"]) + " configuration\n"
         buf += s
-        
+        s = "Path:"+str(path['path']) + "\n"
+        buf += s
+        s = "Type:"+str(path['pathtype']) + "\n"
+        buf += s
+        s = "User:" + str(path['username']) + "\n"
+        buf += s
+        s = "Password:" + str(path['password']) + "\n"
+        buf += s
+
+        nameglobs = path['globexclusion_name'].split('\r')
+        s = "Name exclude globs:" + ','.join(nameglobs) + "\n"
+        buf += s
+
+        nameregex = path['regexexclusion_name'].split('\r')
+        s = "Name exclude regex:" + ','.join(nameregex) + "\n"
+        buf += s
+
+        pathglobs = path['globexclusion_path'].split('\r')
+        s = "Path exclude globs:" + ','.join(pathglobs) + "\n"
+        buf += s
+
+        pathregex = path['regexexclusion_path'].split('\r')
+        s = "Path exclude regex:" + ','.join(pathregex) + "\n"
+        buf += s
+
+        buf += "-----------\n"
+
+    print buf
+
